434e1fe5e7e9b15c8328dfa1196989614af208f3
spandy-r <86184883+spandy-r@users.noreply.github.com>
2021-09-03 15:49:14 +0530

    Add files via upload

---
+++ Assignment4.1.py
@@ -0,0 +1,10 @@
+list=[]
+n=int(input("Enter no. of elements"))
+for i in range(0,n):
+    ele=[int(input("enter element"))]
+    list.append(ele)
+x=max(list)
+print(x)
+                    
+                    
+                    
---
+++ Assignment4.2.py
@@ -0,0 +1,4 @@
+list=[12,3,5,6,4,8,1]
+list.sort() #ascending order
+list.sort(reverse=True)#descending order
+print(list)
